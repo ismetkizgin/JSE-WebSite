aa9f151d1f97e7641082376cf9683ad8caf52d44
ismet kizgin <info@ismetkizgin.com>
2020-11-02 19:52:09 +0300

    fix: role bugs fixed

--- src/app/components/layouts/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/app/components/layouts/admin/admin-sidebar/admin-sidebar.component.ts
@@ -20,7 +20,7 @@ export class AdminSidebarComponent implements OnInit {
       if (
         !item.authorize ||
         item.authorize.indexOf(
-          this._authService.currentUserValue.result.UserStatusName
+          this._authService.currentUserValue.result.UserTypeName
         ) != -1
       )
         this.menu.push(item);
