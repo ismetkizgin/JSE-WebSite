771b92b830419f8f4808d3c59383397bd2a30860
ismet kizgin <info@ismetkizgin.com>
2020-11-04 00:05:28 +0300

    feat: added client navbar and banner component

---
+++ src/app/components/layouts/client/client-navbar/client-navbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientNavbarComponent } from './client-navbar.component';
+
+describe('ClientNavbarComponent', () => {
+  let component: ClientNavbarComponent;
+  let fixture: ComponentFixture<ClientNavbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ClientNavbarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClientNavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
