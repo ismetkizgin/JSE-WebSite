9806689b907bde53a62711e752aee9e901a16bcb
ismet kizgin <info@ismetkizgin.com>
2020-11-08 14:20:42 +0300

    refactor: blog card component revised

--- src/app/components/blog-card/blog-card.component.ts
+++ src/app/components/blog-card/blog-card.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BlogService } from '../../utils/services';
 import { Blog } from '../../models';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
-  styleUrls: ['./blog-card.component.scss']
+  styleUrls: ['./blog-card.component.scss'],
 })
 export class BlogCardComponent implements OnInit {
+  constructor(private _blogService: BlogService) {}
 
-  constructor(
-    private _blogService: BlogService,
-  ) { }
-
-  blogs:Blog=new Blog();
-    async ngOnInit() {
-      try {
-        this.blogs = <Blog>await this._blogService.listAsync();
-      } catch (error) {
-        this._blogService.errorNotification(error);
-      }
+  blogs: Array<Blog>;
+  @Input() limit: number = 6;
+  async ngOnInit() {
+    try {
+      this.blogs = <Array<Blog>>await this._blogService.listAsync({ limit: 6 });
+    } catch (error) {
+      this._blogService.errorNotification(error);
     }
-
+  }
 }
