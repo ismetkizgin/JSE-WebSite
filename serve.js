e12e8f93963d697c0b99be8c1de04255bc336814
ismet kizgin <info@ismetkizgin.com>
2020-11-11 14:42:06 +0300

    refactor: project rename

--- serve.js
+++ serve.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/adminlte-starting-kit-angularjs'));
+app.use(express.static(__dirname + '/dist/jse-website'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/adminlte-starting-kit-angularjs/index.html'));
+res.sendFile(path.join(__dirname+'/dist/jse-website/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
